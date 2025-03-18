09881f32670c7f9295d6b56be7a328f9b97878d4
Norberto Suarez <114125989+norbcode@users.noreply.github.com>
2025-03-18 18:46:18 -0500

    Add Actualizacion app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Funcion para agregrar Amigo
 let amigos = [];
 
 function agregarAmigo() {
@@ -11,3 +13,46 @@ function agregarAmigo() {
     }
     botonAgregar.disabled = true;
 }
+
+// Funcion para actualizar la lista
+function actualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach((nombre) => {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });
+}
+
+// Funcion para sortear amigo
+function sortearAmigo() {
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    if (amigos.length > 0) {
+        const ganador = amigos[Math.floor(Math.random() * amigos.length)];
+        let li = document.createElement("li");
+        li.textContent = "Tu amigo secreto es: " + ganador;
+        resultado.appendChild(li);
+
+// Limpiar la lista después del sorteo
+      
+      limpiarLista();
+    } else {
+        alert("Agrega al menos un participante para realizar el sorteo.");
+    }
+}
+
+function limpiarLista() {
+    amigos = [];
+    actualizarLista();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const input = document.getElementById("amigo");
+    const botonAgregar = document.querySelector(".button-add");
+
+    input.addEventListener("input", function () {
+        botonAgregar.disabled = input.value.trim() === "";
+    });
+});
